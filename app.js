5063c810f1927490158c306b95f65d63f934c4c8
Alycia Moncrieff <alycia@amoncrieff.com>
2016-07-07 16:28:49 -0700

    Simplify sample code. Will explain build and destroy functions in future documentation.

--- app.js
+++ app.js
@@ -1,50 +1,5 @@
 (function($) {
 
-	$.winSize = "";
-
-	// create element that tracks media query size
-    $('body').append('<div id="mq"></div>');
-
-    function setWindowSize() {
-        $.winSize = parseInt($('#mq').css('z-index'));
-    }
-
-    function checkSupportedScripts() {
-        setWindowSize();
-
-        if ($.winSize < 3) {
-            //run mobile only scripts
-            mobileScripts();
-        } else {
-            //run desktop only scripts
-            desktopScripts();
-        }
-    }
-
-    function desktopScripts() {
-    	if (typeof $.testTabs != 'undefined' || $.testTabs != null) {
-    		//destroy tabs
-            $.testTabs.destroy();
-    	}
-    }
-
-    function mobileScripts() {
-    	if (typeof $.testTabs == 'undefined' || $.testTabs == null) {
-        	$.testTabs = tabSet('.c-tabs');
-        } else {
-        	//rebuild tabs
-            $.testTabs.build();
-        }
-    }
-
-    function onResizeScripts() {
-        checkSupportedScripts();
-    }
-
-    $(window).resize($.debounce(400, onResizeScripts));
-
-    $(document).ready(function(){
-    	checkSupportedScripts();
-    });
+    $.testTabs = tabSet('.c-tabs');
 
 })(window.jQuery);
